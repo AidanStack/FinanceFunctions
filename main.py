97baac3f46380b58e90b290b20c937365c3d6de0
Aidan Stack <78806930+AidanStack@users.noreply.github.com>
2021-03-11 09:37:47 -0500

    Added Variance and Standard Deviation Calculators

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ print(compound_interest_calc(80000, .03, 1, 1))
 
 #The function call below tracks a salary that starts 80,000 and increases 3% per year, and returns the value in year 5.
 def salary_by_year(year):
-  print((80000 * (( 1 + (.03))**year)))
-salary_by_year(5)
+  return '{:.2f}'.format((80000 * (( 1 + (.03))**year)))
+print('Starting at 80k, with a 3% raise per year, by year 5 the salary will be',salary_by_year(5))
 
 
 
@@ -95,18 +95,66 @@ def display_as_percentage(val):
   string_val = str(rounded_val)
   return string_val + '%'
 
+# compact function for displaying interest rates (usually in the format of .05) as a percentage
 display_as_percentage = lambda val: '{}%'.format(str(round(val * 100, 1)))
 
-print(display_as_percentage(rate_of_return))
+print('0.075 displayed as a percentage is', display_as_percentage(rate_of_return))
 
-
+# Simple ROI calculator
 def return_on_investment(earnings, initial_investment):
-  return (earnings-initial_investment) / initial_investment
+  return '{:.2f}'.format(earnings-initial_investment / initial_investment)
 
 print('The ROI is', return_on_investment(47,984))
 
-def variance(list):
+# This variance calculator uses the true length of the list as the denominator, meaning it is only to be used when
+# n = population. If it used to calculate variance for a sample, it will be the biased sample variance.
+def population_variance(list):
+  mean = sum(list) / len(list)
+  differences = []
   for i in range(len(list)):
+    differences.append((list[i] - mean) ** 2)
+  return '{:.2f}'.format(sum(differences) / len(list))
+
+print('The population variance is',population_variance([3,6,4,7,8,9,4,2,1,3,4]))
+
+# This variance is the unbiased sample variance
+
+def sample_variance(list):
+  mean = sum(list) / len(list)
+  differences = []
+  for i in list:
+    differences.append((i - mean) ** 2)
+  return '{:.2f}'.format(sum(differences) / (len(list) - 1))
+
+print('The sample variance is', sample_variance([3,6,4,7,8,9,4,2,1,3,4]))
+
+def population_standard_deviation(list):
+  mean = sum(list) / len(list)
+  differences = []
+  for i in range(len(list)):
+    differences.append((list[i] - mean) ** 2)
+  variance = sum(differences) / len(list)
+  return '{:.2f}'.format(variance ** .5)
+
+print('The population standard deviation is',population_standard_deviation([3,6,4,7,8,9,4,2,1,3,4]))
+
+def sample_standard_deviation(list):
+  mean = sum(list) / len(list)
+  differences = []
+  for i in range(len(list)):
+    differences.append((list[i] - mean) ** 2)
+  variance = sum(differences) / (len(list) -1)
+  return '{:.2f}'.format(variance ** .5)
+
+print('The sample standard deviation is', sample_standard_deviation([3,6,4,7,8,9,4,2,1,3,4]))
+
+
+
+
+
+
+
+
 
 
 
